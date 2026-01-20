74ec8bc1d523ca4294b36a392be263245e27615f
Caio Cesar Dos Santos Isernhagen <cisernhagen@maisaedu.com.br>
2026-01-20 11:02:18 -0300

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/my-money-hub/", // ADICIONE ESTA LINHA
+  // Se for produção, usa o nome do repo. Se for dev, usa a raiz '/'
+  base: mode === "production" ? "/my-money-hub/" : "/",
   server: {
     host: "::",
     port: 8080,
